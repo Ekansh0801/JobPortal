c7279b6e5c77995644fb2470316373f9de749206
Ekansh0801 <135941717+Ekansh0801@users.noreply.github.com>
2025-01-05 23:48:17 +0530

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './components/ui/shared/Navbar'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Signup from './components/auth/Signup'
+import Login from './components/auth/Login'
+import Home from './components/Home'
+
+const appRouter = createBrowserRouter([
+  {
+    path:'/',
+    element:<Home/>
+  },
+  {
+    path:'/login',
+    element:<Login/>
+  },
+  {
+    path:'/signup',
+    element:<Signup/>
+  },
+])
+
+function App() {
+
+  return (
+    <>
+      <RouterProvider router={appRouter}/>
+    </>
+  )
+}
+
+export default App
